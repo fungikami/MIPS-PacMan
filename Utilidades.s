993aa8845e648b89ba25067d8dc698e8c308234b
fungikami <kafung09@gmail.com>
2021-12-21 00:38:36 -0400

    Doc

--- Utilidades.s
+++ Utilidades.s
@@ -6,10 +6,12 @@
 
 # Funcion: Convierte una coordenada (x, y) en una 
 #          direccion del Bitmap Display.
-# Entrada: $a0: Direccion base del MAT.
+# Entrada: $a0: Direccion base del Bitmap Display 
+#               correspondiente a (x, y).
 #          $a1: Coordenada x.
 #          $a2: Coordenada y.
-# Salida:  $v0: Direccion del MAT.    
+# Salida:  $v0: Direccion del Bitmap Display 
+#               correspondiente a (x, y).    
 # Planificacion de registros:
 # $t0: Auxiliar
 coord_a_dir_bitmap: 
@@ -18,6 +20,7 @@ coord_a_dir_bitmap:
     move $fp,  $sp
     addi $sp,  $sp, -4
 
+    # Formula: (32*(31 - y) + x)*4 + MAT
     li  $t0, 31
     sub $v0, $t0, $a2   # a = 31 - y
     sll $v0, $v0, 5     # a = a * 32
